27be2e8104dc0139566a957e488a3637f22b33c8
Harsh Mangal <harsh.mangal1108@gmail.com>
2021-12-20 00:47:00 +0530

    Model Basics

--- devfolio/projects/models.py
+++ devfolio/projects/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models #ALL ABM
+import uuid
 
 # Create your models here.
+class Project(models.Model): #Class name is basically table name
+    title = models.CharField(max_length=200) # these are columns
+    description = models.TextField(null=True,blank=True) # Null means DB mai emppty ho skta hai, blank means Form Post karna hai to empty se kar skte hai
+    id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False) #
+    demo_link = models.CharField(max_length=2000,null=True,blank=True)
+    source_code = models.CharField(max_length=2000,null=True,blank=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):  #Due to this funcn on Admin page we get name of records at plcae of objects previously
+        return self.title
+
